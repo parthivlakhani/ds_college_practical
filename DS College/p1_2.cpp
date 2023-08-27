5a543441693cbce299ec83fc16aabdf272c2ec6a
parthivlakhani <107111841+parthivlakhani@users.noreply.github.com>
2023-08-27 22:13:44 +0530

    Update p1_2.cpp

--- DS College/p1_2.cpp
+++ DS College/p1_2.cpp
@@ -17,14 +17,15 @@ int main(){
     }
     cout<<"After Removing Duplicate elements: "<<endl;
     for(i=0;i<size;i++){
-        for(j=i+1;j<size;j++)
+        for(j=i+1;j<size;j++){
             if(arr[i]==arr[j]){
-            for(k=j;k<=size;k++){
-                arr[k]=arr[k+1];
+                for(k=j;k<=size;k++){
+                    arr[k]=arr[k+1];
             }
             size--;
             j--;
         }
+    }
 
     }
     for(i=0;i<size;i++){
